a8965356caf18e2f40ab97ac6441a178fbd863b0
bilin <bilinskij2001@gmail.com>
2021-08-07 13:39:47 +0300

    Added recursion algorithms

--- recursion.py
+++ recursion.py
@@ -19,6 +19,18 @@ def find_max_element(nums: list):
         return nums[0] if nums[0] > find_max_element(nums[1:]) else find_max_element(nums[1:])
 
 
+def binary_search_recursion(nums, val):
+    a, b = 0, len(nums) - 1
+    middle = (a + b) // 2
+    if nums[middle] == val:
+        return middle
+    elif nums[middle] < val:
+        return middle + 1 + binary_search_recursion(nums[middle + 1:], val)
+    else:
+        return binary_search_recursion(nums[:middle], val)
+
+
 print(sum_nums([3, 3, 4, 7, 9, 1]))
 print(find_amount_of_elements([3, 6, 4, 9, 12, 5, 9]))
 print(find_max_element([3, 6, 4, 9, 12, 5, 9]))
+print(binary_search_recursion(list(range(10)), 0))
